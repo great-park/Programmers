5bb4a867269a8dd163064d9b4568c18bf68a6028
Park Chanho <cksgh1735@gmail.com>
2023-09-04 12:34:40 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,40 @@
+def solution(brown, yellow):
+    answer = []
+    total_block = brown + yellow
+    
+    for b in range(1, total_block+1):
+        if total_block%b == 0:
+            a = total_block // b
+            if a < b:
+                continue
+                
+            if yellow == (a-2)*(b-2):
+                answer.append(a)
+                answer.append(b)
+
+    return answer
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+# def solution(brown, yellow):
+#     answer = []
+#     total = brown + yellow                  # a * b = total
+#     for b in range(1,total+1):
+#         if (total / b) % 1 == 0:            # total / b = a
+#             a = total / b
+#             if a >= b:                      # a >= b
+#                 if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
+#                     return [a,b]
+            
+#     return answer
