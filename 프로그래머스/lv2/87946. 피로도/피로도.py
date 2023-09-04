2952ceecbc2ecb5d639390f01f4481deedac1787
Park Chanho <cksgh1735@gmail.com>
2023-09-04 12:47:15 +0900

    [level 2] Title: 피로도, Time: 0.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -2,20 +2,50 @@ from itertools import permutations
 
 def solution(k, dungeons):
     answer = 0
-    size = len(dungeons)
+    per_list = []
+    for i in range(1, len(dungeons)+1):
+        per_list += list(permutations(dungeons))
     
-    for i in range(1, size+1):
-        for nums in permutations(dungeons, i):
-            temp_k = k
-            temp_cnt = 0
-            success = True
-            for num in nums:
-                if num[0] <= temp_k and temp_k >= num[1]:
-                    temp_k -= num[1]
-                else:
-                    success = False
-            if success:
-                answer = len(nums)
-    
-    
+    for schedule in per_list:
+        tmp_k, tmp_answer = k, 0
+        for dungeon in schedule:
+            min_need, cost = dungeon[0], dungeon[1]
+            if min_need > tmp_k:
+                break
+            else:
+                tmp_k -= cost
+                tmp_answer += 1
+        answer = max(answer, tmp_answer)
+
     return answer
+
+
+
+
+
+
+
+
+
+
+# from itertools import permutations
+
+# def solution(k, dungeons):
+#     answer = 0
+#     size = len(dungeons)
+    
+#     for i in range(1, size+1):
+#         for nums in permutations(dungeons, i):
+#             temp_k = k
+#             temp_cnt = 0
+#             success = True
+#             for num in nums:
+#                 if num[0] <= temp_k and temp_k >= num[1]:
+#                     temp_k -= num[1]
+#                 else:
+#                     success = False
+#             if success:
+#                 answer = len(nums)
+    
+    
+#     return answer
