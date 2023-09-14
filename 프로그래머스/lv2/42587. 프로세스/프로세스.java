0d5aa0103cb4b620f03d3b6c611b9e8ed218820f
Park Chanho <cksgh1735@gmail.com>
2023-09-14 17:53:42 +0900

    [level 2] Title: 프로세스, Time: 0.55 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -1,35 +1,53 @@
 import java.util.*;
 
 class Solution {
-    static int cur_max_p = 0;
     public int solution(int[] priorities, int location) {
         int answer = 0;
         PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
         
-        for(int i=0; i<priorities.length; i++) {
-            q.add(priorities[i]);
+        for(int i=0; i<priorities.length;i++){
+            q.offer(priorities[i]);
         }
         
         while(!q.isEmpty()) {
-            for(int i=0; i<priorities.length;i++) {
-                if(priorities[i] == q.peek()) {
+            for(int i=0; i<priorities.length; i++){
+                if(!q.isEmpty() && q.peek() == priorities[i]) {
+                    q.poll();
+                    answer++;
                     if (i == location) {
-                        answer++;
                         return answer;
                     }
-                    answer++;
-                    q.poll();
                 }
             }
-            
-            
         }
-        
-        
-        
-        
         
         return answer;
     }
-    
 }
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] priorities, int location) {
+//         int answer = 0;
+//         PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        
+//         for(int i=0; i<priorities.length; i++) {
+//             q.add(priorities[i]);
+//         }
+        
+//         while(!q.isEmpty()) {
+//             for(int i=0; i<priorities.length;i++) {
+//                 if(priorities[i] == q.peek()) {
+//                     if (i == location) {
+//                         answer++;
+//                         return answer;
+//                     }
+//                     answer++;
+//                     q.poll();
+//                 }
+//             }   
+//         }
+//         return answer;
+//     }
+// }
