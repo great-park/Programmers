2f4c20367f0efb1fefaf55d8e50b5ab3a4ddde7d
Park Chanho <cksgh1735@gmail.com>
2023-09-14 18:33:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.89 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,39 @@
 import java.util.*;
 
 class Solution {
-    
-    int[] dx = {1, 0, -1, 0};
-    int[] dy = {0, 1, 0, -1};
-    
+    int[] dx = {1,0,-1,0};
+    int[] dy = {0,1,0,-1};
     public int solution(int[][] maps) {
         int answer = 0;
-        
         int[][] visited = new int[maps.length][maps[0].length];
-        
         bfs(maps, visited);
-        answer = visited[maps.length-1][maps[0].length-1];
-        
+        answer = visited[maps.length - 1][maps[0].length - 1];
         if(answer == 0){
-            answer = -1;
+            return -1;
         }
-        
         return answer;
     }
-    
-    public void bfs(int[][] maps, int[][] visited){
-        visited[0][0] = 1;
-        Queue<int[]> q = new LinkedList<int[]>();
+    public void bfs(int[][] maps, int[][] visited) {
+        Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{0,0});
+        visited[0][0] = 1;
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-            
-            for(int i=0; i<4; i++) {
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-                if (0 <= nx & nx < maps[0].length & 0 <= ny & ny < maps.length) {
-                    if (visited[ny][nx] == 0 && maps[ny][nx] == 1){
-                        q.offer(new int[]{nx,ny});
+            for (int i=0;i<4;i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (0<=nx && nx<maps[0].length && 0<=ny && ny<maps.length){
+                    if (visited[ny][nx] == 0 && maps[ny][nx] == 1) {
+                        q.offer(new int[]{nx, ny});
                         visited[ny][nx] = visited[y][x] + 1;
                     }
                 }
-                
             }
         }
     }
 }
+
+
