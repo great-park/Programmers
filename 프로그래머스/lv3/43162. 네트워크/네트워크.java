d1d02cb0dec2b48c797168bf4cd5546642a12521
Park Chanho <cksgh1735@gmail.com>
2023-09-14 18:19:00 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,38 +1,34 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
-    static boolean[] chk;
+    boolean[] visited;
     Queue<Integer> q = new LinkedList<Integer>();
-    
+    int answer = 0;
+      
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        chk = new boolean[n];
-    
-        for(int i=0; i<n; i++) {
-            if(!chk[i]){
-                bfs(computers, i, n);
+        visited = new boolean[n];
+        
+        for(int i=0;i<n;i++){
+            if(!visited[i]) {
+                bfs(i, computers, n);
                 answer++;
-            }
+            } 
         }
         return answer;
     }
-
-    void bfs(int[][] computers, int start, int n) {
-        
+    
+    public void bfs(int start, int[][] computers, int n) {
         q.offer(start);
-        chk[start] = true;
+        visited[start] = true;
         
-        while(!q.isEmpty()){
-            int v = q.poll();
-            for(int j = 0; j<n;j++) {
-                if(!chk[j]  && computers[v][j] == 1) {
-                
-                    q.offer(j);
-                    chk[j] = true;
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            for (int next=0; next< n; next++) {
+                if (!visited[next] && computers[node][next] == 1) {
+                    q.offer(next);
+                    visited[next] = true;
                 }
             }
         }
-        
     }
 }
