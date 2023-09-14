596287eb43b38823edb21595f009609d5d2e898b
Park Chanho <cksgh1735@gmail.com>
2023-09-14 16:40:19 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.82 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,40 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new LinkedList<>();
-		int sum = 0;
-		int time = 0; 
+    class Truck {
+        int weight;
+        int move;
 
-		for(int i = 0; i < truck_weights.length; i++) { // 향상된 for문을 쓰는게 좋을 것 
-			int truck = truck_weights[i];
+        public Truck(int weight) {
+            this.weight = weight;
+            this.move = 1;
+        }
 
-			while(true) {
-				// 큐에 아무것도 없는 경우 = 어떠한 트럭도 다리위에 없음 
-				if(queue.isEmpty()) { 
-					queue.add(truck);
-					sum += truck;
-					time++; // 다리에 오를 때만 시간 추가 
-					break;
-				} else if(queue.size() == bridge_length) { // 큐에 다리 길이만큼 트럭이 다 찬 경우 
-					sum -= queue.poll();
-				} else  { // 다리 길이만큼 큐가 차지않았음
-					// weight 값을 넘지 않는 선에서 새로운 트럭을 다리에 올려줌 
-					if(sum + truck <= weight) {
-						queue.add(truck);
-						sum += truck;
-						time++;
-						break;
-					} else { 
-						// 넘는다면 0을 넣어 이미 큐에 있는 트럭이 다리를 건너게 만듬 
-						queue.add(0);
-						time++;
-					}
-				}
-			}
-		}
+        public void moving() {
+            move++;
+        }
+    }
 
-        // 마지막 트럭에서 반복문이 끝나는데, 마지막 역시 다리 길이만큼 지나가야하기에 + 다리 길이 
-		return time + bridge_length; 
+    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+        Queue<Truck> waitQ = new LinkedList<>();
+        Queue<Truck> moveQ = new LinkedList<>();
+
+        for (int t : truckWeights) {
+            waitQ.offer(new Truck(t));
+        }
+
+        int answer = 0;
+        int curWeight = 0;
+
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
+
+            if (moveQ.isEmpty()) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+                continue;
+            }
+
+            for (Truck t : moveQ) {
+                t.moving();
+            }
+
+            if (moveQ.peek().move > bridgeLength) {
+                Truck t = moveQ.poll();
+                curWeight -= t.weight;
+            }
+
+            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+            }
+        }
+
+        return answer;
     }
 }
