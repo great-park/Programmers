d8404fa2664ad2017efd5b81f499c9747bd5cf55
Park Chanho <cksgh1735@gmail.com>
2023-10-10 01:05:09 +0900

    [level 2] Title: 더 맵게, Time: 1716.95 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,17 +1,53 @@
-import heapq
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+import heapq as hq
 
 def solution(scoville, K):
+
+    hq.heapify(scoville)
     answer = 0
-    heap = []
-    for s in scoville:
-        heapq.heappush(heap, s)
-    
-    while heap[0] < K:
-        heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap)*2)
-        answer+=1
-    
-        if len(heap) == 1 and heap[0] < K:
-            answer = -1
+    while True:
+        first = hq.heappop(scoville)
+        if first >= K:
             break
-    
+        if len(scoville) == 0:
+            return -1
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1  
+
     return answer
+
+# import heapq
+
+# def solution(scoville, K):
+#     answer = 0
+#     heap = []
+#     for s in scoville:
+#         heapq.heappush(heap, s)
+    
+#     while heap[0] < K:
+#         heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap)*2)
+#         answer+=1
+    
+#         if len(heap) == 1 and heap[0] < K:
+#             answer = -1
+#             break
+    
+#     return answer
