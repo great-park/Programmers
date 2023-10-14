2c9b97a282706b422258db20ce394985aa1b3d74
Park Chanho <cksgh1735@gmail.com>
2023-10-15 01:15:49 +0900

    [level 2] Title: 소수 찾기, Time: 0.65 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+
+def is_prime(number):
+    if number < 2:
+        return False
+    
+    for i in range(2, number):
+        flag = number%i
+        if flag == 0:
+            return False
+    return True
+
+def solution(numbers):
+    answer = []
+    nums = [n for n in numbers]
+    per_list = []
+    for i in range(1, len(nums)+1):
+        per_list += list(permutations(nums, i))
+    number_list = list(set(int(("").join(p)) for p in per_list))
+    
+    for test_num in number_list:
+        if is_prime(test_num):
+            answer.append(test_num)
+    
+    return len(answer)
