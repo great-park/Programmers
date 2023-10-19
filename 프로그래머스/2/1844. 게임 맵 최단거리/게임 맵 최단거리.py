4bda20b87656110f83ac941f5acf48e315ea1c64
Park Chanho <cksgh1735@gmail.com>
2023-10-20 04:56:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.23 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,50 +2,47 @@ from collections import deque
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
-# def solution(maps):
-#     answer = 0
-#     n, m = len(maps), len(maps[0])
-#     visited = [[0]*m for _ in range(n)]
-#     queue = deque()
-#     queue.append([0,0])
-#     visited[0][0] = 1
-    
-#     while queue:
-#         x,y = queue.popleft()
-        
-#         for i in range(4):
-#             nx, ny = x+dx[i], y+dy[i]
-#             if 0<=nx<m and 0<=ny<n and visited[ny][nx] == 0:
-#                 if maps[ny][nx] == 1:
-#                     queue.append([nx,ny])
-#                     visited[ny][nx] = visited[y][x] + 1
-
-                        
-#     if visited[n-1][m-1] != 0:
-#         return visited[n-1][m-1]
-#     else:
-#         return -1
-
 def solution(maps):
     n,m = len(maps), len(maps[0])
     visited = [[0]*m for _ in range(n)]
-    queue = deque([(0,0)])
+    q = deque()
+    q.append([0,0])
     visited[0][0] = 1
     
-    while queue:
-        x,y = queue.popleft()
-        print(x,y)
-        
+    while q:
+        x,y = q.popleft()
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            
-            if 0<= nx < m and 0 <= ny < n and visited[ny][nx] == 0 and maps[ny][nx] == 1:
-                queue.append([nx,ny])
+            if 0<=nx<m and 0<=ny<n and not visited[ny][nx] and maps[ny][nx] == 1:
+                q.append([nx,ny])
                 visited[ny][nx] = visited[y][x] + 1
                 
     if visited[n-1][m-1] != 0:
         return visited[n-1][m-1]
     else:
         return -1
+
+
+# def solution(maps):
+#     n,m = len(maps), len(maps[0])
+#     visited = [[0]*m for _ in range(n)]
+#     queue = deque([(0,0)])
+#     visited[0][0] = 1
+    
+#     while queue:
+#         x,y = queue.popleft()
+#         print(x,y)
+        
+#         for i in range(4):
+#             nx, ny = x+dx[i], y+dy[i]
+            
+#             if 0<= nx < m and 0 <= ny < n and visited[ny][nx] == 0 and maps[ny][nx] == 1:
+#                 queue.append([nx,ny])
+#                 visited[ny][nx] = visited[y][x] + 1
+                
+#     if visited[n-1][m-1] != 0:
+#         return visited[n-1][m-1]
+#     else:
+#         return -1
     
     
