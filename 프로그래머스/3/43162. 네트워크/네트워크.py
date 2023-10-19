6c53663ac7dc43709c2a4c6ed53eebad80ed088d
Park Chanho <cksgh1735@gmail.com>
2023-10-20 04:50:40 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,15 @@
 from collections import deque
 
-def BFS(start_id, visited, computers):
-    queue = deque()
-    queue.append(start_id)
-    visited[start_id] = True
+def BFS(i, visited, computers):
+    visited[i] = True
+    q = deque()
+    q.append(i)
     
-    while queue:
-        cur_id = queue.popleft()
-        
+    while q:
+        cur_id = q.popleft()
         for new_id, value in enumerate(computers[cur_id]):
             if new_id != cur_id and not visited[new_id] and value == 1:
-                queue.append(new_id)
+                q.append(new_id)
                 visited[new_id] = True
     
     
@@ -18,9 +17,11 @@ def BFS(start_id, visited, computers):
 def solution(n, computers):
     answer = 0
     visited = [False for _ in range(n)]
-    for id in range(n):
-        if not visited[id]:
-            BFS(id, visited, computers)
+    
+    for i in range(n):
+        if not visited[i]:
+            BFS(i, visited, computers)
             answer += 1
+
     return answer
 
